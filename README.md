b7b84d02920fbc98f9c5b664178454da67953a7a
Andrew Lane <ajlane50@gmail.com>
2017-12-13 22:46:04 +0000

    added twitterTrawler function and a configuration file to go with it, specifying rules to save tweets to a database

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Twitter Analyser
 
 Conducts network analysis based on interactions between users using a particular hashtag
+
+Leaders and followers refer to the two sets of nodes in a bipartite graph in which
+followers tweet about leaders.
---
+++ trawlTweets.js
@@ -0,0 +1,6 @@
+/*
+Make a call to the twitter API, continuously retrieving tweets from the public
+stream which conform to rules specified in the config.json file. These retrieved
+have been identified as interesting for subsequent analysis, and are written to
+a sqlite database.
+*/
